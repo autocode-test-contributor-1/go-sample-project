8afcb45c356c9881888d57e74aabbc540ea6b744
Kirill Peshin <kirill_peshin@epam.com>
2021-06-30 17:26:33 +0300

    Upd

--- calculator.go
+++ calculator.go
@@ -9,7 +9,24 @@ func multiply(x int, y int) int {
 }
 
 func subtraction(x int, y int) int {
+	//self-assigned variable
+	x = x
+
+	//duplicated string literals
+	var a string = "Hello"
+	var b string = "Hello"
+	var c string = "Hello"
+	if a == "Hello" && b == "Hello" && c == "Hello" {
 	return x - y
+	} else if a == "Hello" && b == "Hello" && c == "Hello" { //same condition
+	return x - y
+	} else {
+	return 1
+	}
+}
+
+//empty function
+func doNothing() {
 }
 
 func divide(x int, y int) int {
