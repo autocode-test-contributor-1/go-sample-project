35f2b9c877374a7a7b2c581d3984db39286b7b9f
Kirill Peshin <kirill_peshin@epam.com>
2021-06-30 15:08:17 +0300

    Upd

--- calculator.go
+++ calculator.go
@@ -9,9 +9,12 @@ func multiply(x int, y int) int {
 }
 
 func subtraction(x int, y int) int {
-	return x - y
+	return x - y + 2
 }
 
 func divide(x int, y int) int {
+	if true {
+	y = 0
+	}
 	return x / y
 }
